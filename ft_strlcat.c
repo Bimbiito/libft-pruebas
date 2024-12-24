47681dbdc6379fb453d24aa7a3744c097512e3d1
Bimbiito <llorente.fatimaworks@gmail.com>
2024-12-24 15:02:50 +0100

    refactor ft_strlcat to simplify length calculation and remove unused main function

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -17,10 +17,9 @@ size_t ft_strlcat(char *dest,const char *src, size_t n)
     size_t i;
     size_t x;
     size_t len_total;
-    
+
 	i = ft_strlen(dest);
 	x = 0;
-	len_total = ft_strlen(dest) + ft_strlen(src);
     if(n == 0)
         return(0);
     // while(i < n-1 || dest[i] != '\0')
@@ -29,16 +28,11 @@ size_t ft_strlcat(char *dest,const char *src, size_t n)
     {
         dest[i++] = src[x++];
     }
-    return (len_total);    
+ 
+    return (ft_strlen(dest) + ft_strlen(src));    
 }
 
 int main()
 {
-	char dest[20] = "hola";
-	const char *src;
-	size_t n;
-	src = ", mundo";
-	n = 5;
-	
-	printf("%zu\n %s\n", ft_strlcat(dest, src, n), dest);
+  
 }
--- libft.h
+++ libft.h
@@ -24,6 +24,6 @@ int		ft_strlen(const char *str);
 void	ft_memset(void *ptr, int value, size_t num);
 void	ft_bzero(void *s, size_t n);
 void	ft_memcpy(void *dest, const void *src, size_t n);
-size_t	ft_memmove(void *dest, const void *src, size_t n);
+void	ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
